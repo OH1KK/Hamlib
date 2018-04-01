7fe5f8e96680546d686dd417bc8147f71f151461
Kari Karvonen <github@toimii.fi>
2018-04-01 19:02:04 +0300

    Update yaesu.h

--- yaesu/yaesu.h
+++ yaesu/yaesu.h
@@ -77,5 +77,6 @@ extern const struct rig_caps frg9600_caps;
 extern const struct rig_caps vr5000_caps;
 extern const struct rig_caps vx1700_caps;
 extern const struct rig_caps ft1200_caps;
+extern const struct rig_caps ftdx3000_caps;
 
 #endif /* _YAESU_H */
